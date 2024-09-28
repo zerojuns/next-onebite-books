55a376a9ab7af1d9c994eef7becb3231215d38f8
youwater <cacaros98@naver.com>
2024-09-28 11:51:43 +0900

    2024-09-28 8일차

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,12 +1,34 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext,  InferGetStaticPropsType } from "next";
 import style from "./[id].module.css"
 import fecthOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
 
-export const getServerSideProps = async (
-    context : GetServerSidePropsContext
+export const getStaticPaths = () =>{
+    return {
+        paths: [
+            {params:{id :"1"}},
+            {params:{id :"2"}},
+            {params:{id :"3"}},
+        ],
+        fallback : true,
+        // false : 404 NotFound
+        // blocking : SSR 방식
+        // true : SSR 방식 + 데이터가 없는 풀백 상태의 페이지 부터 반환
+    };
+};
+
+export const getStaticProps = async (
+    context : GetStaticPropsContext
 ) =>{
     const id = context.params!.id;
     const book = await fecthOneBook(Number(id));
+
+    if(!book){
+        return{
+            notFound:true,
+        };
+    }
+
     return{
         props : {
             book,
@@ -15,8 +37,10 @@ export const getServerSideProps = async (
 };
 
 export default function Page(
-    {book} : InferGetServerSidePropsType<typeof getServerSideProps>
+    {book} : InferGetStaticPropsType<typeof getStaticProps>
 ){
+    const router = useRouter();
+    if(router.isFallback) return "로딩중입니다";
     if(!book) return "문제가 발생했습니다";
 
     const {title,subTitle,description,author,publisher,coverImgUrl} = book;
